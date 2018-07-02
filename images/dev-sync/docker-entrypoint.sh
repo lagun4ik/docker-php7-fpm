b2055e0f1bbfde6fbb56de62ec27c8d81eacd52f
Ivan Lagunovsky <ivan@lagunovsky.com>
2018-07-02 17:41:32 +0300

    fix bugs

--- images/dev-sync/docker-entrypoint.sh
+++ images/dev-sync/docker-entrypoint.sh
@@ -4,20 +4,19 @@ set -e
 
 VENDOR_SYNC_CRON_SCHEDULE=${VENDOR_SYNC_CRON_SCHEDULE:-*/30 * * * *}
 
-if [ "$1" == 'composer-sync' ]; then
+mkdir -p ${VENDOR_DESTINATION}
 
+if [ "$1" == 'composer-sync' ]; then
     filecount=`find "$VENDOR_DESTINATION" -type f | wc -l`
     if [ ${filecount} -eq 0 ];
     then
         echo "full sync the vendors is need"
+        sudo chown -R $(id -u -n):$(id -u -n) ${VENDOR_DESTINATION}
         composer-sync
     fi
-
     echo -e "$VENDOR_SYNC_CRON_SCHEDULE composer-sync" | crontab -
     echo "sync the vendors by cron is starting..."
     exec crond -f
 else
-
     exec "$@"
-
 fi
